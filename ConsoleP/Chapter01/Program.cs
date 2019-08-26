742e9da5232f6dee8a42990bab2c10143149628a
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-08-26 13:17:42 +0900

    8.26

---
+++ ConsoleP/Chapter01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Chapter01
+{
+  internal class Program
+  {
+    public static void Main(string[] args)
+    {
+      int num = 1234567;
+      float num2 = 3.141592f;
+      float f1 = new System.Single();
+      Console.WriteLine(f1);
+      Console.WriteLine(num.GetType());
+      Console.WriteLine(num2.GetType());
+
+      Console.WriteLine("num1: {0, 10}, num2: {1,2:F}", num, num2);
+    }
+  }
+}
--- ConsoleP/HelloWorld2/Program.cs
+++ ConsoleP/HelloWorld2/Program.cs
@@ -1,13 +1,21 @@
 ﻿using System;
+using static System.Console;
 
 namespace HelloWorld2
 {
-    internal class Program
+  internal class Program
+  {
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, {0}!", "World");
-        }
+      Console.WriteLine("Hello, World!");
+      // Console.WriteLine("Hello, {0}!", args[0]);
+      // Console.ReadKey();
+
+      Write("여러분 ");
+      WriteLine("안녕하세요");
+      
+      WriteLine("\n");
+      
     }
+  }
 }
