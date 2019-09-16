b11171bcae520f73686f2bc9f137465128f78c17
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-09-16 12:30:28 +0900

    9.16

--- Interface/Interface01/Program.cs
+++ Interface/Interface01/Program.cs
@@ -1,9 +1,42 @@
-﻿namespace Interface01
+﻿using System;
+
+namespace Interface01
 {
+  internal interface ILogger // 인터페이스 이름은 보통 대문자 I로 시작
+  {
+    void WriteLog(string log);
+  }
+
+  // 인터페이스를 상속받은 클래스는 인터페이스의 멤버들을 구현해야 한다
+  internal class ConsoleLogger : ILogger
+  {
+    public void WriteLog(string log)
+    {
+      Console.WriteLine(log);
+    }
+
+    public void DerivedMethod()
+    {
+      Console.WriteLine("DerivedMethod()");
+    }
+  }
+  
   internal class Program
   {
     public static void Main(string[] args)
     {
+      ConsoleLogger c1 = new ConsoleLogger();
+      c1.WriteLog("Hello World!");
+      
+      ILogger c2 = new ConsoleLogger();
+      c2.WriteLog("Hi C#");
+      if (c2 is ConsoleLogger)
+      {
+        ConsoleLogger cl = c2 as ConsoleLogger;
+        ConsoleLogger cl2 = (ConsoleLogger) c2;
+        cl.DerivedMethod();
+        cl2.DerivedMethod();
+      }
     }
   }
 }
---
+++ Interface/Interface02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Interface02
+{
+  internal interface IProduct
+  {
+    string ProductName { get; set; }
+    int Price { get; set; }
+  }
+
+  internal class Product : IProduct
+  {
+    private string productName;
+
+    public string ProductName
+    {
+      get => productName;
+      set => productName = value;
+    }
+
+    public int Price { get; set; }
+  }
+  
+  internal class Program
+  {
+    public static void Main(string[] args)
+    {
+      Product p = new Product();
+      p.ProductName = "노트북";
+      p.Price = 2_000_000;
+      Console.WriteLine($"{p.ProductName}, {p.Price}");
+    }
+  }
+}
