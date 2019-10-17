8be7aa095a6f2521a68d301583284a78d76bc0e8
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-10-17 16:21:28 +0900

    10.17

--- File/File01/Program.cs
+++ File/File01/Program.cs
@@ -7,8 +7,11 @@ namespace File01
   {
     static void Main(string[] args)
     {
-      CreateDirectoryUsingDirectory();
+      // CreateDirectoryUsingDirectory();
+      CreateDirectoryUsingDirectoryInfo();
     }
+
+    #region Directory, File 클래스
 
     private static void CreateDirectoryUsingDirectory()
     {
@@ -16,6 +19,7 @@ namespace File01
 
       if (Directory.Exists(path))
       {
+        DeleteFileUsingFile(path);
         Directory.Delete(path);
         Console.WriteLine($"Directory {path} Deleted");
       }
@@ -23,7 +27,89 @@ namespace File01
       {
         Directory.CreateDirectory(path);
         Console.WriteLine($"Directory {path} Created");
+        CreateFileUsingFile(path);
       }
     }
+
+    private static void DeleteFileUsingFile(string path)
+    {
+      path += @"\Test.txt";
+      if (File.Exists(path)) 
+      {
+        File.Delete(path); 
+        Console.WriteLine($"File Deleted at {path}");
+      }
+    }
+
+    private static void CreateFileUsingFile(string path)
+    {
+      path += @"\Test.txt";
+      if (!File.Exists(path))
+      {
+        FileStream fs = File.Create(path);
+        StreamWriter sw = new StreamWriter(fs);
+        sw.WriteLine("Hello");
+        sw.WriteLine("File");
+        sw.Close();
+        Console.WriteLine($"File Created at {path}");
+      }
+    }
+
+    #endregion
+
+    #region DirectoryInfo, FileInfo 클래스
+
+    
+    private static void CreateDirectoryUsingDirectoryInfo()
+    {
+      string dirPath = @"D:\CodingData\Learn-CSharp\TestDirectoryInfo";
+      string filePath = dirPath + @"\TestInfo.txt";
+      
+      DirectoryInfo dirInfo = new DirectoryInfo(dirPath);
+      FileInfo fileInfo = new FileInfo(filePath);
+
+      if (dirInfo.Exists)
+      {
+        DeleteFileUsingFileInfo(fileInfo);
+        dirInfo.Delete();
+        Console.WriteLine($"Directory Deleted. {dirPath}");
+      }
+      else
+      {
+        dirInfo.Create();
+        Console.WriteLine($"Directory Created. {dirPath}");
+        CreateFileUsingFileInfo(fileInfo);
+        ReadFileUsingFileInfo(fileInfo);
+      }
+    }
+
+    private static void ReadFileUsingFileInfo(FileInfo fileInfo)
+    {
+      StreamReader sr = fileInfo.OpenText();
+      string s;
+      while ((s = sr.ReadLine()) != null)
+      {
+        Console.WriteLine(s);
+      }
+      sr.Close();
+      Console.WriteLine($"File has been read. {fileInfo.FullName}");
+    }
+
+    private static void DeleteFileUsingFileInfo(FileInfo fileInfo)
+    {
+      fileInfo.Delete();
+      Console.WriteLine($"File Deleted. {fileInfo.FullName}");
+    }
+
+    private static void CreateFileUsingFileInfo(FileInfo fileInfo)
+    {
+      StreamWriter sw = fileInfo.CreateText();
+      sw.WriteLine("안녕하세요");
+      sw.WriteLine("내일은 축제");
+      sw.Close();
+      Console.WriteLine($"File Created. {fileInfo.FullName}");
+    }
+
+    #endregion
   }
 }
---
+++ File/File02/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace File02
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string directory;
+
+      directory = args.Length < 1 ? "." : args[0];
+
+      Console.WriteLine($"{directory} directory info");
+      Console.WriteLine("- Directories: ");
+
+      foreach (var item in Directory.GetDirectories(directory))
+      {
+        Console.WriteLine(item);
+      }
+      
+      var directories = (from dir in Directory.GetDirectories(directory)
+        let info = new DirectoryInfo(dir)
+        select new {Name = info.Name, Attribute=info.Attributes}).ToList();
+
+
+      foreach (var dir in directories)
+      {
+        Console.WriteLine($"{dir.Name}: {dir.Attribute}");
+      }
+
+      Console.WriteLine("- Files: ");
+      // 쿼리 만들기
+      var files = from file in Directory.GetFiles(directory)
+        let info = new FileInfo(file)
+        select new {Name = info.Name, FileSize = info.Length, Attribute = info.Attributes};
+
+      foreach (var item in files)
+      {
+        Console.WriteLine($"{item.Name}, {item.FileSize}, {item.Attribute}");
+      }
+    }
+  }
+}
