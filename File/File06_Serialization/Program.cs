ca6be3aa2be5219c115fb8cde8d75764f795eff5
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-10-21 22:58:04 +0900

    10.21

---
+++ File/File06_Serialization/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace File06_Serialization
+{
+  [Serializable]
+  class NameCard
+  {
+    public string Name;
+    public string Phone;
+    public int Age;
+  }
+  
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Stream ws = new FileStream("a.txt", FileMode.Create);
+      // 객체를 직렬화/역직렬화하는 클래스
+      BinaryFormatter serializer = new BinaryFormatter();
+      
+      NameCard nc = new NameCard();
+      nc.Name = "아이유";
+      nc.Phone = "010-444-4444";
+      nc.Age = 28;
+      
+      // 클래스 직렬화 -> FileStream에 기록
+      serializer.Serialize(ws, nc); // nc 객체를 직렬화하여 ws에 작성
+      ws.Close();
+      
+      // FileStream에서 클래스 역직렬화
+      Stream rs = new FileStream("a.txt", FileMode.Open);
+      BinaryFormatter deserializer = new BinaryFormatter();
+
+      NameCard nc2;
+      nc2 = deserializer.Deserialize(rs) as NameCard;
+      rs.Close();
+
+      Console.WriteLine(nc2.Name);
+      Console.WriteLine(nc2.Phone);
+      Console.WriteLine(nc2.Age);
+    }
+  }
+}
---
+++ Thread/Thread01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+// Thread (명령어를 실행하기 위한 스케줄링 단위로 프로세스 내부에서 생성 가능)
+namespace Thread01
+{
+  class Program
+  {
+    // 메인 스레드
+    static void Main(string[] args)
+    {
+      // 스레드의 존재 확인
+      Thread thread = Thread.CurrentThread; // 현재 실행 중인 스레드
+      Console.WriteLine(thread.ThreadState);
+      
+      // Thread.Sleep() : Running 상태인 스레드를 지정된 시간만큼 중지
+      for (int i = 0; i < 3; i++)
+      {
+        Console.WriteLine(DateTime.Now);
+        Thread.Sleep(1000);
+      }
+      
+      // 새로운 스레드 생성
+      Thread t = new Thread(ThreadFunc);
+      // t.IsBackground = true; // 메인이 끝나면 종료됨
+      t.Start();
+      t.Join(); // 스레드가 완료될 때까지 대기
+      
+      Console.WriteLine("하아하아 죽여버렸어");
+    }
+
+    private static void ThreadFunc()
+    {
+      Thread.Sleep(1000);
+      Console.WriteLine("하아하아 죽어버릴것만 같아");
+    }
+  }
+}
